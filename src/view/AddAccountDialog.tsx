0c6c843ad6f7d8a73192dc75e7dae2e827e3a90f
luca.liu <luca.liu@quectel.com>
2025-04-22 13:56:18 +0800

    完成添加账号弹框

--- src/view/AddAccountDialog.tsx
+++ src/view/AddAccountDialog.tsx
@@ -6,12 +6,19 @@ import {
     TouchableOpacity,
     Image,
     Modal,
+    TextInput,
 } from 'react-native';
 // @ts-ignore
 import icon_close from '../assets/icon_close_modal.png';
 
 const addAccount = React.forwardRef(function AddAccountDialog(props, ref) {
     const [display, setDisplay] = useState(false);
+    const [select, setselect] = useState(0);
+    const [designationState, setDesignation] = useState('');
+    const [accountState, setAccount] = useState('');
+    const [passwordState, setPassword] = useState('');
+
+    const saveSubmit = () => {};
 
     const show = () => {
         setDisplay(true);
@@ -25,6 +32,191 @@ const addAccount = React.forwardRef(function AddAccountDialog(props, ref) {
             hideAdd: hide,
         };
     });
+
+    const renderTitle = () => {
+        const styles = StyleSheet.create({
+            title: {
+                marginTop: 10,
+                fontSize: 14,
+            },
+            close: {
+                marginRight: 10,
+                marginTop: 5,
+                width: 30,
+                height: 30,
+                right: 0,
+                position: 'absolute',
+            },
+            closeImg: {
+                tintColor: '#00000033',
+                width: 30,
+                height: 30,
+            },
+            root: {
+                flexDirection: 'row',
+                justifyContent: 'center',
+                width: '100%',
+            },
+        });
+        return (
+            <View style={styles.root}>
+                <Text style={styles.title}>{'添加账号'}</Text>
+                <TouchableOpacity
+                    style={styles.close}
+                    activeOpacity={0.7}
+                    onPress={hide}>
+                    <Image style={styles.closeImg} source={icon_close} />
+                </TouchableOpacity>
+            </View>
+        );
+    };
+    const renderChoose = () => {
+        const array = ['游戏', '平台', '银行卡', '其他'];
+
+        const getButton = () => {
+            return array.map((item, index) => {
+                return (
+                    <TouchableOpacity
+                        key={`item=${item}`}
+                        style={[
+                            styles.touch,
+                            index === 0
+                                ? styles.leftRadius
+                                : index === 3
+                                ? styles.rightRadius
+                                : '',
+                            {
+                                marginLeft: index === 0 ? 0 : -1,
+                                backgroundColor:
+                                    select === index ? '#3599f7' : '#ffffff',
+                            },
+                        ]}
+                        onPress={() => {
+                            setselect(index);
+                        }}>
+                        <Text
+                            style={[
+                                styles.txt,
+                                {
+                                    color:
+                                        select === index
+                                            ? '#ffffff'
+                                            : '#000000',
+                                },
+                            ]}
+                            key={`item=${item}`}>
+                            {item}
+                        </Text>
+                    </TouchableOpacity>
+                );
+            });
+        };
+
+        const styles = StyleSheet.create({
+            root: {
+                marginTop: 10,
+                marginRight: 10,
+                marginLeft: 10,
+                flexDirection: 'row',
+                height: 40,
+            },
+            touch: {
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderWidth: 1,
+                borderColor: '#EDEDED',
+                flex: 1,
+                height: 40,
+            },
+            txt: {
+                fontSize: 10,
+            },
+            leftRadius: {
+                borderTopLeftRadius: 6,
+                borderBottomLeftRadius: 6,
+            },
+            rightRadius: {
+                borderBottomRightRadius: 6,
+                borderTopRightRadius: 6,
+            },
+        });
+
+        return <View style={styles.root}>{getButton()}</View>;
+    };
+    const txtInput = (index: number) => {
+        const styles = StyleSheet.create({
+            root: {},
+            txtInput: {
+                borderRadius: 6,
+                marginRight: 10,
+                marginTop: 8,
+                height: 40,
+                marginLeft: 10,
+                backgroundColor: '#ededed',
+                fontSize: 10,
+            },
+        });
+
+        return (
+            <View style={styles.root}>
+                <TextInput
+                    style={styles.txtInput}
+                    value={
+                        index === 1
+                            ? designationState
+                            : index === 2
+                            ? accountState
+                            : index === 3
+                            ? passwordState
+                            : ''
+                    }
+                    onChangeText={(text: string) => {
+                        switch (index) {
+                            case 1:
+                                setDesignation(text);
+                                break;
+                            case 2:
+                                setAccount(text);
+                                break;
+                            case 3:
+                                setPassword(text);
+                                break;
+                        }
+
+                        console.log(designationState);
+                    }}
+                />
+            </View>
+        );
+    };
+    const saveButton = () => {
+        const styles = StyleSheet.create({
+            root: {
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginBottom: 16,
+                marginTop: 16,
+                backgroundColor: '#3599f7',
+                borderRadius: 6,
+                marginLeft: 10,
+                marginRight: 10,
+                height: 40,
+            },
+            txt: {
+                color: '#ffffff',
+                fontSize: 14,
+            },
+        });
+
+        return (
+            <TouchableOpacity
+                style={styles.root}
+                activeOpacity={0.7}
+                onPress={saveSubmit}>
+                <Text style={styles.txt}>{'保存'}</Text>
+            </TouchableOpacity>
+        );
+    };
     return (
         <Modal
             visible={display}
@@ -36,13 +228,16 @@ const addAccount = React.forwardRef(function AddAccountDialog(props, ref) {
             }}>
             <View style={[styles.root, StyleSheet.absoluteFill]}>
                 <View style={styles.viewRoot}>
-                    <Text style={styles.title}>{'添加账号'}</Text>
-                    <TouchableOpacity
-                        style={styles.close}
-                        activeOpacity={0.7}
-                        onPress={hide}>
-                        <Image style={styles.closeImg} source={icon_close} />
-                    </TouchableOpacity>
+                    {renderTitle()}
+                    <Text style={styles.txt}>{'账号类型'}</Text>
+                    {renderChoose()}
+                    <Text style={styles.txt}>{'账号名称'}</Text>
+                    {txtInput(1)}
+                    <Text style={styles.txt}>{'账号'}</Text>
+                    {txtInput(2)}
+                    <Text style={styles.txt}>{'密码'}</Text>
+                    {txtInput(3)}
+                    {saveButton()}
                 </View>
             </View>
         </Modal>
@@ -56,28 +251,15 @@ const styles = StyleSheet.create({
         backgroundColor: '#00000033',
     },
     viewRoot: {
+        flexDirection: 'column',
         borderRadius: 20,
-        alignItems: 'center',
         backgroundColor: '#ffffff',
         width: '80%',
-        height: '10%',
     },
-    title: {
+    txt: {
         marginTop: 10,
-        fontSize: 14,
-    },
-    close: {
-        marginRight: 10,
-        marginTop: 5,
-        width: 30,
-        height: 30,
-        right: 0,
-        position: 'absolute',
-    },
-    closeImg: {
-        tintColor: '#00000033',
-        width: 30,
-        height: 30,
+        marginLeft: 10,
+        fontSize: 10,
     },
 });
 
